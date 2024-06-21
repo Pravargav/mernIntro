71d74bc8e9907e0b935e4dc1ad2980a0d032a1df
Pravargav <96449025+Pravargav@users.noreply.github.com>
2024-06-21 12:03:11 +0530

    ygigi
    ylvil

--- client2/src/App.js
+++ client2/src/App.js
@@ -2,7 +2,7 @@ import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Navbar } from "./components/navbar";
 import { Shop } from "./pages/shop/shop";
-import { Bid } from "./pages/bid/bid"
+import { Bid } from "./pages/bid/bid";
 import { ShopContextProvider } from "./context/shop-context";
 import { VerifyAdmin } from "./verify/verifyAdmin";
 
@@ -10,16 +10,14 @@ function App() {
   return (
     <div className="App">
       <ShopContextProvider>
-
-             <Router>
-               <Navbar />
-               <Routes>
-               <Route path="/" element={<VerifyAdmin />} />
-                 <Route path="/user" element={<Shop />} />
-                 <Route path="/admin" element={<Bid />} />
-               </Routes>
-             </Router>
-
+        <Router>
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<VerifyAdmin />} />
+            <Route path="/user" element={<Shop />} />
+            <Route path="/admin" element={<Bid />} />
+          </Routes>
+        </Router>
       </ShopContextProvider>
     </div>
   );
